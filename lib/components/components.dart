69af6669a61a08d1c4dfd604cc1de72d5bd66260
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-11-04 09:05:55 +0200

    Add nested list view

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,1 +1,3 @@
 export 'today_recipe_list.dart';
+export 'friend_post_tile.dart';
+export 'friend_post_list.dart';
--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/api/api_service.dart';
+import 'package:fooderlich/components/friend_post_list.dart';
 import 'package:fooderlich/components/today_recipe_list.dart';
 import 'package:fooderlich/models/explore_data.dart';
 
@@ -13,11 +14,18 @@ class ExploreScreen extends StatelessWidget {
     return FutureBuilder(
       future: mockService.getExploreData(),
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-        // TODO: Add Nested List Views
-        // 4
         if (snapshot.connectionState == ConnectionState.done) {
-          final recipes = snapshot.data?.todayRecipes ?? [];
-          return TodayRecipeListView(recipes: recipes);
+          return ListView(
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
+              Container(
+                height: 400,
+                color: Colors.green,
+              ),
+            ],
+          );
         } else {
           return const Center(
             child: CircularProgressIndicator(),
