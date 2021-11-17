652496a2025bb37f4c9de676e23a1ab8cf1fadf0
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-11-17 21:40:14 +0200

    Manage tab state

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:fooderlich/home.dart';
+import 'models/tab_manager.dart';
 import 'themes/fooderlich_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const Fooderlich());
@@ -17,7 +19,13 @@ class Fooderlich extends StatelessWidget {
     return MaterialApp(
       theme: theme,
       title: 'Fooderlich',
-      home: const Home(),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          // TODO 10: Add GroceryManager Provider
+        ],
+        child: const Home(),
+      ),
     );
   }
 }
