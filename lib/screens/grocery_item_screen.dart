47461a172364537d7fa276f02fea289625050d4d
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-11-18 21:24:22 +0200

    Add basic grocery item screen

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -24,12 +24,76 @@ class GroceryItemScreen extends StatefulWidget {
 }
 
 class _GroceryItemScreenState extends State<GroceryItemScreen> {
-  // TODO: Add grocery item screen state properties
+  final nameController = TextEditingController();
+  String name = '';
+  Importance importance = Importance.low;
+  DateTime dueDate = DateTime.now();
+  TimeOfDay timeOfDay = TimeOfDay.now();
+  Color color = Colors.pink;
+  int currentSliderValue = 0;
+
+  @override
+  void initState() {
+    final originalItem = widget.originalItem;
+    if (originalItem != null) {
+      nameController.text = originalItem.name;
+      name = originalItem.name;
+      currentSliderValue = originalItem.quantity;
+      importance = originalItem.importance;
+      color = originalItem.color;
+      final date = originalItem.date;
+      timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+      dueDate = date;
+    }
+
+    nameController.addListener(() {
+      setState(() {
+        name = nameController.text;
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO 12: Add GroceryItemScreen Scaffold
-    return Container(color: Colors.orange);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.check),
+            onPressed: () {
+              // TODO 24: Add callback handler
+            },
+          )
+        ],
+        elevation: 0.0,
+        title: Text(
+          'Grocery Item',
+          style: GoogleFonts.lato(fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            // TODO 13: Add name TextField
+            // TODO 14: Add Importance selection
+            // TODO 15: Add date picker
+            // TODO 16: Add time picker
+            // TODO 17: Add color picker
+            // TODO 18: Add slider
+            // TODO: 19: Add Grocery Tile
+          ],
+        ),
+      ),
+    );
   }
 
   // TODO: Add buildNameField()
