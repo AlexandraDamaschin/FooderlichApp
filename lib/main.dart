f22c33bfc05b74f5fb83601961d2faf9c8b06674
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-11-20 17:23:21 +0200

    Update app with profile screen and new libs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:fooderlich/home.dart';
+import 'package:fooderlich/screens/home.dart';
 import 'package:fooderlich/models/grocery_manager.dart';
-import 'models/tab_manager.dart';
+import 'package:fooderlich/screens/splash_screen.dart';
+import 'models/profile_manager.dart';
 import 'themes/fooderlich_theme.dart';
 import 'package:provider/provider.dart';
 
@@ -10,22 +11,48 @@ void main() {
   runApp(const Fooderlich());
 }
 
-class Fooderlich extends StatelessWidget {
+class Fooderlich extends StatefulWidget {
   const Fooderlich({Key? key}) : super(key: key);
+  @override
+  _FooderlichState createState() => _FooderlichState();
+}
+
+class _FooderlichState extends State<Fooderlich> {
+  final _groceryManager = GroceryManager();
+  final _profileManager = ProfileManager();
+  // TODO: Create AppStateManager
+  // TODO: Define AppRouter
+
+  // TODO: Initialize app router
 
   @override
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.dark();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => _groceryManager,
+        ),
+        ChangeNotifierProvider(
+          create: (context) => _profileManager,
+        ),
+        // TODO: Add AppStateManager ChangeNotifierProvider
+      ],
+      child: Consumer<ProfileManager>(
+        builder: (context, profileManager, child) {
+          ThemeData theme;
+          if (profileManager.darkMode) {
+            theme = FooderlichTheme.dark();
+          } else {
+            theme = FooderlichTheme.light();
+          }
 
-    return MaterialApp(
-      theme: theme,
-      title: 'Fooderlich',
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => TabManager()),
-          ChangeNotifierProvider(create: (context) => GroceryManager()),
-        ],
-        child: const Home(),
+          return MaterialApp(
+            theme: theme,
+            title: 'Fooderlich',
+            // TODO: Replace with Router widget
+            home: const SplashScreen(),
+          );
+        },
       ),
     );
   }
