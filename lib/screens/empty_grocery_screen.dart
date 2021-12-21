c6e88a93b6ec189a8423fb68e112708be5ed8284
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-12-21 14:51:14 +0200

    Flutter build runner will generate serialization for class

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class EmptyGroceryScreen extends StatelessWidget {
   const EmptyGroceryScreen({Key? key}) : super(key: key);
